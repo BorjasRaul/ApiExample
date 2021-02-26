13dcefeaed640a80ecb7a993bcb4e4bb9fab1719
BorjasRaul <79670821+BorjasRaul@users.noreply.github.com>
2021-02-26 12:36:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/apiexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.apiexample
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.apiexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
