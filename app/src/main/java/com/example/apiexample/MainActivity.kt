b388f12c305fcbcbdc02cfe7d2416515747f628a
BorjasRaul <79670821+BorjasRaul@users.noreply.github.com>
2021-03-01 10:13:11 -0600

    hola

--- app/src/main/java/com/example/apiexample/MainActivity.kt
+++ app/src/main/java/com/example/apiexample/MainActivity.kt
@@ -16,12 +16,11 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-//raul
+
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
     private lateinit var binding : ActivityMainBinding
     private lateinit var adapter: DogsAdapter
     private val dogImage= mutableListOf<String>()
-    //hola
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
